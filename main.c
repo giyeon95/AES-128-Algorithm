fc93b52d0e7b246f7ae6955480ab5506738e1a9f
KIYEON KIM <giyeon15@nate.com>
2019-05-16 20:23:20 +0900

    aes full

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 typedef unsigned char BYTE;
 
 const unsigned int S_BOX[16][16] =
-        //0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
+//0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
         { 0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
           0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
           0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
@@ -21,8 +21,23 @@ const unsigned int S_BOX[16][16] =
           0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
           0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
 
-const unsigned int RoundKey[16] = {
-                               0xa0,0xfa,0xfe,0x17,0x88,0x54,0x2c,0xb1,0x23,0xa3,0x39,0x39,0x2a,0x6c,0x76,0x05
+
+BYTE cyperKey[16] = {
+        0x2b, 0x7e, 0x15,0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+};
+
+
+BYTE rcon[40] = {
+        0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00,
+        0x04, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00,
+        0x10, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
+        0x40, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00,
+        0x1b, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00
+};
+
+
+const unsigned int RoundKey[16] = { //나중에 제거
+        0xa0,0xfa,0xfe,0x17,0x88,0x54,0x2c,0xb1,0x23,0xa3,0x39,0x39,0x2a,0x6c,0x76,0x05
 };
 
 const unsigned int mixColumnKey[16] = {
@@ -36,11 +51,13 @@ const unsigned int mixColumnKey[16] = {
 void subByte(BYTE *input, BYTE *output);
 void shiftRow(BYTE *input, BYTE *output);
 void mixColumns(BYTE *input, BYTE *output);
+void keySchedule(BYTE *input, int round, BYTE *output);
 void addRoundKey(BYTE *input, BYTE *output);
 
 
 /**HELP SHOUCE FUNCTION**/
 
+BYTE *rotAndSubBytes(BYTE *arr);
 BYTE x_time(BYTE b, BYTE n);
 void reverse(BYTE *arr, int start, int end);
 void shiftLeft(BYTE *arr, int d, int n);
@@ -48,32 +65,60 @@ void print4by4(BYTE *arr);
 
 
 
-BYTE firstSubByteResult[16] = {0,};
-BYTE secondShiftRowsResult[16] = {0,};
-BYTE thirdMixColumnsResult[16] = {0,};
-BYTE fourAddRoundKeyResult[16] = {0,};
+BYTE firstSubByteResult[16] = { 0, };
+BYTE secondShiftRowsResult[16] = { 0, };
+BYTE thirdMixColumnsResult[16] = { 0, };
+BYTE fourAddRoundKeyResult[16] = { 0, };
+BYTE roundKey[16] = { 0, };
+
 
 int main() {
-    BYTE firstSubBytes[16] = {0x19,0x3d,0xe3,0xbe,0xa0,0xf4,0xe2,0x2b,0x9a,0xc6,0x8d,0x2a,0xe9,0xf8,0x48,0x08};
+    BYTE firstSubBytes[16] = { 0x32,0x43,0xf6,0xa8,0x88,0x5a,0x30,0x8d,0x31,0x31,0x98,0xa2,0xe0,0x37,0x07,0x34 };
+    //BYTE firstSubBytes[16] = { 0x19,0x3d,0xe3,0xbe,0xa0,0xf4,0xe2,0x2b,0x9a,0xc6,0x8d,0x2a,0xe9,0xf8,0x48,0x08 };
+    addRoundKey(firstSubBytes, firstSubByteResult);
 
 
-    //printf("1. subByte\n");
-    subByte(firstSubBytes,firstSubByteResult);
-    //print4by4(firstSubByteResult);
+    printf("\n - round 1 input - \n");
+    print4by4(firstSubByteResult);
 
-    printf("2. shiftRows\n");
-    shiftRow(firstSubByteResult, secondShiftRowsResult);
-    print4by4(secondShiftRowsResult);
+    for(int i = 0 ; i < 10 ; i++) {
 
-    printf("3. MixColumns\n");
-    mixColumns(secondShiftRowsResult,thirdMixColumnsResult);
-    //print4by4(thirdMixColumnsResult);
+        subByte(firstSubByteResult, firstSubByteResult);
+
+        printf(" - %d after of SubBytes\n",i+1);
+        print4by4(firstSubByteResult);
 
 
+        shiftRow(firstSubByteResult, secondShiftRowsResult);
 
-    //printf("4. addRoundKey\n");
-    addRoundKey(thirdMixColumnsResult, fourAddRoundKeyResult);
-    print4by4(fourAddRoundKeyResult);
+        printf(" - %d after of ShiftRow \n",i+1);
+        print4by4(secondShiftRowsResult);
+
+
+
+
+        keySchedule(cyperKey, i, cyperKey);
+
+        printf(" - %d  Round Key \n",i+1);
+        print4by4(cyperKey);
+
+
+        if(i != 9) {
+            mixColumns(secondShiftRowsResult, thirdMixColumnsResult);
+            addRoundKey(thirdMixColumnsResult, firstSubByteResult);
+
+            printf(" - %d  After of MixColumnd Key \n",i+1);
+            print4by4(thirdMixColumnsResult);
+        } else {
+            addRoundKey(secondShiftRowsResult, firstSubByteResult);
+        }
+
+
+
+        printf(" - %d outPut \n", i+1);
+        print4by4(firstSubByteResult);
+    }
+
     return 0;
 
 }
@@ -83,10 +128,10 @@ int main() {
 
 void subByte(BYTE *input, BYTE *output) {
     int i, j;
-    BYTE be[2] = {0,};
+    BYTE be[2] = { 0, };
     BYTE mask = 0x0F;
 
-    for(i = 0 ; i < 16 ; i++) {
+    for (i = 0; i < 16; i++) {
         be[0] = input[i] >> 4; // 앞 4자리
         be[1] = (input[i] & mask); // 뒤 4자리
         output[i] = S_BOX[be[0]][be[1]];
@@ -95,32 +140,32 @@ void subByte(BYTE *input, BYTE *output) {
 
 void shiftRow(BYTE *input, BYTE *output) {
     int i, j;
-    BYTE tmpArr[4] = {0,};
+    BYTE tmpArr[4] = { 0, };
 
-         for(i = 0 ; i< 4 ; i++) {
+    for (i = 0; i < 4; i++) {
 
-            for(j = 0 ; j< 4 ; j++) {
-                tmpArr[j] = input[i+j*4];
-            }
-            shiftLeft(tmpArr, i, 4);
+        for (j = 0; j < 4; j++) {
+            tmpArr[j] = input[i + j * 4];
+        }
+        shiftLeft(tmpArr, i, 4);
 
-            for(j = 0 ; j < 4 ; j++) {
-                output[i+j*4] = tmpArr[j];
-            }
-       }
+        for (j = 0; j < 4; j++) {
+            output[i + j * 4] = tmpArr[j];
+        }
+    }
 }
 
 void mixColumns(BYTE *input, BYTE *output) {
-    int i=0, j,k = 0;
+    int i = 0, j, k = 0;
     BYTE temp = 0;
 
-    for(i= 0 ; i< 16 ; i++) {
-        for(j= 0 ; j< 4 ; j++) {
-            temp ^= x_time(mixColumnKey[(i%4)*4+j],input[k*4+j]);
+    for (i = 0; i < 16; i++) {
+        for (j = 0; j < 4; j++) {
+            temp ^= x_time(input[k * 4 + j], mixColumnKey[(i % 4) * 4 + j]);
         }
         output[i] = temp;
         temp = 0;
-        if(i!=0 && i%4 == 3) k++;
+        if (i != 0 && i % 4 == 3) k++;
     }
 }
 
@@ -128,32 +173,75 @@ BYTE x_time(BYTE b, BYTE n) {
     int i;
     BYTE temp = 0, mask = 0x01;
 
-    for(i = 0 ; i< n ; i++) {
-        if(n & mask) {
+    for (i = 0; i < n; i++) {
+        if (n & mask) {
             temp ^= b;
         }
-        if(b & 0x80) {
-            b = (b<<1) ^ 0x1b;
+        if (b & 0x80) {
+            b = (b << 1) ^ 0x1b;
         }
         else {
-            b <<=1;
+            b <<= 1;
         }
         mask <<= 1;
     }
     return temp;
 }
 
+void keySchedule(BYTE *input, int round, BYTE *output) {
+    int i, j;
+    BYTE * rotArr;
+    for (i = 0; i < 4; i++) {
+        if (!i) {
+            rotArr = rotAndSubBytes(input);
+            for (j = 0; j < 4; j++) {
+                output[j] = input[j] ^ *(rotArr + j) ^ rcon[4 * round + j];
+
+            }
+        }
+        else {
+            for (j = 0; j < 4; j++) {
+                output[4 * i + j] = input[4 * i + j] ^ output[4 * (i - 1) + j];
+            }
+        }
+    }
+}
+
+BYTE *rotAndSubBytes(BYTE *arr) {
+    int i, temp;
+    BYTE rot[4] = { 0, };
+    BYTE be[2] = { 0, };
+    BYTE mask = 0x0F;
+
+    temp = arr[12];
+
+    for (i = 1; i < 4; i++) {
+        rot[i - 1] = arr[i + 12];
+    }
+    rot[3] = temp;
+
+
+
+    for (i = 0; i < 4; i++) {
+        be[0] = rot[i] >> 4; // 앞 4자리
+        be[1] = (rot[i] & mask); // 뒤 4자리
+        rot[i] = S_BOX[be[0]][be[1]];
+    }
+
+    return rot;
+}
+
 void addRoundKey(BYTE *input, BYTE *output) {
     int i;
-    for( i = 0 ; i< 16 ; i++) {
-        output[i] = input[i] ^ RoundKey[i];
+    for (i = 0; i < 16; i++) {
+        output[i] = input[i] ^ cyperKey[i];
     }
 }
 
 void reverse(BYTE *arr, int start, int end) {
     BYTE temp;
     end = end - 1;
-    while(start < end) {
+    while (start < end) {
         temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
@@ -171,11 +259,11 @@ void shiftLeft(BYTE *arr, int d, int n) {
 }
 
 void print4by4(BYTE *arr) {
-    int i,j;
+    int i, j;
 
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
-            printf("%02x ", arr[i + j*4]);
+            printf("%02x ", arr[i + j * 4]);
         }
         printf("\n");
     }
